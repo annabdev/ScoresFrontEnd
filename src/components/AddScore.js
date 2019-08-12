3a6cf5d97fc96d58b781b59b5dc73c4912ef6740
Anna Beagley <beagley.dev@gmail.com>
2019-08-12 11:26:35 -0600

    updated styled components

--- src/components/AddScore.js
+++ src/components/AddScore.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import VideoBackground from './VideoBackground';
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 import '../index.css'
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Press Start 2P']
+  }
+});
+
+const GlobalStyles = createGlobalStyle`
+body {
+  @import url('https://fonts.google.com/specimen/Press+Start+2P?selection.family=Press+Start+2P');
+  font-family: 'Press Start 2P', Roboto;
+  color: white;
+}`
+
+const Container = styled.div
+  `display: flex;
+flex-direction: column;
+width: 170px;
+padding: 70px 0;
+align-items: center;
+margin-left: auto;
+justify-content: space-around;
+border: 2px white groove;
+border-radius: 5px;
+`
 
 
 
@@ -9,67 +36,74 @@ import '../index.css'
 
 
 class AddScore extends Component {
-    state = {
-      name: "",
-      score: 0,
-      game: ""
-    };
-  
+  state = {
+    name: "",
+    score: 0,
+    game: ""
+  };
+
   //Post 
   handleSubmit = async e => {
-      e.preventDefault();
-      const data = JSON.stringify(this.state);
-console.log(typeof this.state.score);
-      await fetch("http://localhost:4000/", {
-          method: "POST",
-          body: data,
-          headers: {
-              "Content-Type": "application/json"
-          }
-      });
+    e.preventDefault();
+    const data = JSON.stringify(this.state);
+    console.log(typeof this.state.score);
+    await fetch("http://localhost:4000/", {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
   };
-  
-    render(){
-      console.log(this)
-      console.log(this.state)
+
+  render() {
+    console.log(this)
+    console.log(this.state)
     return (
-       
-        <div>
-      <VideoBackground />
-            Please enter your information here
-      <br />
-      
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
+
+      <div>
+        <GlobalStyles />
+        <Container>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name:
     <input type="text" name="name" onChange={e => this.setState({ name: e.target.value })} />
-                </label>
-                <br />
-                <label>
-                    Score:
+            </label>
+            <br />
+            <br />
+            <label>
+              Score:
                     <input type="number" name="Score" onChange={e => this.setState({ score: parseInt(e.target.value) })} />
-                </label>
+            </label>
+            <br />
+            <br />
+            <label>
+              Game:
                 <br />
-                <label>
-                Game:
-                </label>
-                <select onChange={e => this.setState({ game: e.target.value })}>
-                <option value="Select Game">Select Game</option>
-  <option value="Ms. Pacman">Ms. Pacman</option>
-  <option value="Iron Maiden Pinball">Iron Maiden Pinball</option>
-  <option value="Skeeball">Skeeball</option>
-  <option value="Donkey Kong Jr.">Donkey Kong Jr.</option>
-</select>
-  <input type = "submit"
-    value = "Submit"/>
-     {/* onclick = "changeColor()"  */}
-    
-</form>
-       <br />
-            <Link to="/topscores">Top Scores</Link>
-        </div>
+            </label>
+            <select onChange={e => this.setState({ game: e.target.value })}>
+              <option value="Select Game">Select Game</option>
+              <option value="Ms. Pacman">Ms. Pacman</option>
+              <option value="Iron Maiden Pinball">Iron Maiden Pinball</option>
+              <option value="Skeeball">Skeeball</option>
+              <option value="Donkey Kong Jr.">Donkey Kong Jr.</option>
+            </select>
+            <br />
+            <br />
+            <input type="submit"
+              value="Submit" />
+
+
+          </form>
+          <br />
+
+          <br />
+
+          <Link to="/topscores">Top Scores</Link>
+        </Container>
+      </div>
     );
-}
+  }
 }
 
 export default AddScore;
--- src/components/TopScores.js
+++ src/components/TopScores.js
@@ -1,6 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import WebFont from 'webfontloader';
+import styled from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
+WebFont.load({
+  google: {
+    families: ['Press Start 2P']
+  }
+});
+
+const GlobalStyles = createGlobalStyle`
+body {
+  @import url('https://fonts.google.com/specimen/Press+Start+2P?selection.family=Press+Start+2P');
+  font-family: 'Press Start 2P', Roboto;
+  color: white;
+  font-size: 12px;
+}`
+
+const Container = styled.div
+  `display: flex;
+flex-direction: column;
+width: 300px;
+padding: 70px 0;
+align-items: center;
+margin-left: auto;
+justify-content: space-around;
+border: 2px white groove;
+border-radius: 5px;
+`
+const Scores = styled.div
+`
+display: flex;
+flex-direction: row;
+align-items: left;
+justify-content: space-around;
+`
 
 class TopScores extends Component {
     state = {
@@ -58,13 +93,14 @@ class TopScores extends Component {
       console.log(this.state);
   return (
     <div>
-       
+       <GlobalStyles />
+       <Container>
     {this.state.scores.map( (scores, index, game) => (
      <div className="border" key={index}>
-         <h1>{scores.game}</h1>
-         
-     <h2>{scores.name} : {scores.score}</h2>
-     
+         <h2>{scores.game}</h2>
+         <Scores>
+     <h3>{scores.score}  {scores.name}</h3>
+     </Scores>
     
 
      </div>
@@ -73,7 +109,7 @@ class TopScores extends Component {
       <Link to="/">Add New Score</Link>
      
         
-        
+      </Container>
         </div>
     )}
 }
